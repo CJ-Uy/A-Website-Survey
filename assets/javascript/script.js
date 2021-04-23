01092e40a05c0ddacbba8c96109358d26342ee48
Charles Joshua Uy <75460325+CJ-Uy@users.noreply.github.com>
2021-04-23 21:36:19 +0800

    working nexts

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,37 @@
+var card_index = 1;
+
+function change_card(n) {
+  show_card(card_index += n);  
+}
+function current_card(n) {
+  show_card(card_index = n);
+}
+
+function show_card(n) {
+  var cards = document.getElementsByClassName("survey_card");
+
+  if (n >  cards.length){
+    alert("Insert Graphs");
+  }
+  if (n < 1){
+    card_index = 1;
+  }
+
+  for (var i = 0; i < cards.length; i++){
+    cards[i].style.display = "none";
+  }
+  cards[card_index - 1].style.display = "block";
+}
+
+
+
+show_card(card_index);
+
+
+
+
+
+
 function text() {
   var text = document.getElementById("press");
   var style = {
